157e2ad586520eb83bec88616e5e67e9bae90880
Kris Litman <kris.d.litman@gmail.com>
2021-07-30 16:51:28 -0400

    Form changes zipcode in state when submit button is clicked

--- src/components/zipcodeForm.js
+++ src/components/zipcodeForm.js
@@ -2,21 +2,33 @@ import React from 'react'
 import '../zipcodeForm.css'
 
 class ZipcodeForm extends React.Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
     this.state = {
       zipcode: '',
     }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({zipcode: event.target.value})
+  }
+
+  handleSubmit(event) {
+    alert('Zip code submitted: ' + this.state.zipcode)
+    event.preventDefault()
   }
 
   render() {
     return (
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <input
             type='text'
             placeholder='Enter your zipcode'
             name='zipcode'
             value={this.state.zipcode}
+            onChange={this.handleChange}
           /><br></br>
 
           <button>☕️</button>
