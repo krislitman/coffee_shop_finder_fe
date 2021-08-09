b1735e0b0f1c2bd2777cb460593fcb7c4a934cac
Kris Litman <kris.d.litman@gmail.com>
2021-08-08 20:15:13 -0400

    Added constructor and state to zipcode form class

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css'
 import React from 'react'
+import ZipcodeForm from './components/zipcodeForm'
 //import  Header  from './components/header'
 //import Footer from './components/footer'
 //import MainContent from './components/mainContent'
+
 function App() {
 	return (
 		<div className='App'>
 			<header className='AppHeader'>
 				<h1>Coffee Shop Finder</h1>
 			</header>
+			<ZipcodeForm />
 		<div className='layout'>
 	 		<div>Coffee Shops</div>
 	   	<div>Coffee Shop Details</div>
--- src/components/zipcodeForm.js
+++ src/components/zipcodeForm.js
@@ -1,13 +1,20 @@
 import React from 'react'
 
 class ZipcodeForm extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      isLoading: false,
+      coffeeShops: [],
+    }
+  }
+  
   render() {
     return (
       <React.Fragment>
         <form align="center">
-          <label for='zipcode'>Enter your zipcode:</label><br></br>
           <input type='text' id='zipcode' placeholder='Enter your zipcode' /><br></br>
-          <input type='submit' value='Submit'/>
+          <input type='submit' value='☕️'/>
         </form>
       </React.Fragment>
     )
