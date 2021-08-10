80f4902ffebee3454693c13d8f13fcc836630b75
Kris Litman <kris.d.litman@gmail.com>
2021-08-09 23:00:01 -0400

    API credentials added, working on standalone api call in zipcodeForm.js

--- src/components/zipcodeForm.js
+++ src/components/zipcodeForm.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 
 class ZipcodeForm extends React.Component {
   constructor(){
@@ -8,13 +9,35 @@ class ZipcodeForm extends React.Component {
       coffeeShops: [],
     }
   }
-  
+
+  componentDidMount(){
+   const Key = process.env.REACT_APP_ZIPCODE_API_KEY
+   const response = axios.get('/v3/businesses/search', {
+     params: {
+      location: 33602,
+      categories: 'coffee',
+      sort_by: 'rating',
+   },
+   headers: {
+    Authorization: `${Key}`,
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET',
+    'Access-Control-Allow-Headers': 'Content-Type',
+   },
+  })
+    const data = response.data
+    this.setState({
+      isLoading: true,
+      coffeeShops: data,
+    })
+  }
+
   render() {
     return (
       <React.Fragment>
         <form align="center">
           <input type='text' id='zipcode' placeholder='Enter your zipcode' /><br></br>
-          <input type='submit' value='☕️'/>
+          <button onClick={this.yelpCall}>☕️</button>
         </form>
       </React.Fragment>
     )
